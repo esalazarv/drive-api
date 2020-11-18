18dd86427c06fa3e00fe92157770359e2ec0e667
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-11-18 03:52:07 -0600

    Add Storage module
    [add]
    - aws sdk
    - s3 configuration
    - storage entity, controller, service and DTOs
    - storage migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SettingsModule } from './settings/settings.module';
 import { UsersModule } from './modules/users/users.module';
+import { StorageModule } from './modules/storage/storage.module';
 
 @Module({
-  imports: [SettingsModule, UsersModule],
+  imports: [SettingsModule, UsersModule, StorageModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/storage/entities/storage.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique(['name'])
+export class Storage {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ default: false })
+  public: boolean;
+
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column()
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
